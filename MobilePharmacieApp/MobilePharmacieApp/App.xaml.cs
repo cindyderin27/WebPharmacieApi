e1b77c45a51435a1294ea2912618e2a203dbe7fa
cindy <temetangcindy@gmail.com>
2021-05-26 17:06:21 +0200

    creation du projet mobile et asp

---
+++ MobilePharmacieApp/MobilePharmacieApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MobilePharmacieApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobilePharmacieApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Accueil();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
